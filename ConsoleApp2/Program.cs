91f4e16ef5ad2f4716bbd60eff3be59cde2389f3
ivans <ivans@VANESHFIRESH>
2024-05-10 21:23:37 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.Clear();
+
+            while (true)
+            {
+                Console.Write("Сколько см у тебя хуй: ");
+                int penis = Convert.ToInt32(Console.ReadLine());
+
+                if (penis < 20)
+                {
+                    Console.WriteLine("Капец у тебя маленький стручочек");
+                }
+                else
+                {
+                    Console.WriteLine("Хуя ты конь");
+                }
+            }
+        }
+        
+    }
+}
